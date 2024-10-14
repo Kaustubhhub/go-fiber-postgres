b73b309e1cc93a4dd2bc0803a946570209c45509
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-10-15 02:45:45 +0530

    v1

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"github.com/kaustubh/go-fiber/models"
+	"github.com/kaustubh/go-fiber/storage"
 	"gorm.io/gorm"
 )
 
@@ -27,7 +31,51 @@ func (r *Repository) SetupRoutes(app *fiber.App) {
 	api.Get("/books", r.GetBooks)
 }
 
+func (r *Repository) DeleteBook(context *fiber.Ctx) error {
+	bookModel := &models.Books{}
+	id := context.Params("id")
+
+	if id == "" {
+		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{"message": "id cannot be empty"})
+		return nil
+	}
+
+	err := r.DB.Delete(bookModel, id).Error
+
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(&fiber.Map{"message": "couldn't delete the book"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{"message": "Deleted successfully"})
+
+	return nil
+}
+
+func (r *Repository) GetBookByID(context *fiber.Ctx) error {
+	bookModel := &models.Books{}
+	id := context.Params("id")
+
+	if id == "" {
+		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{"message": "id cannot be empty"})
+		return nil
+	}
+
+	// err := r.DB.First(&bookMode, id).Error
+	err := r.DB.Where("id = ?", id).First(bookModel).Error
+
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(&fiber.Map{"message": "cannot find the book"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{"message": "found the book", "data": bookModel})
+
+	return nil
+}
+
 func (r *Repository) CreateBook(context *fiber.Ctx) error {
+	fmt.Println("in CreateBook")
 	book := Book{}
 	err := context.BodyParser(&book)
 
@@ -76,10 +124,24 @@ func main() {
 		log.Fatal(err)
 	}
 
+	config := &storage.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASSWORD"),
+		User:     os.Getenv("DB_USER"),
+		DBName:   os.Getenv("DB_DBNAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	}
+
 	db, err := storage.NewConnection(config)
 
 	if err != nil {
 		log.Fatal("Could no load the database.")
+	}
+
+	err = models.MigrateBooks(db)
+	if err != nil {
+		log.Fatal("could not migrate db : ", err)
 	}
 
 	r := Repository{
@@ -88,5 +150,6 @@ func main() {
 
 	app := fiber.New()
 	r.SetupRoutes(app)
-	app.Listen(":8080")
+	fmt.Println("reached here")
+	app.Listen(":8081")
 }
